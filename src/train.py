c1d850baf3ba95cd67895bfbe6d4cee38bdf6d96
方一舟 <352453347@qq.com>
2024-07-18 13:25:36 +0800

    修改训练配置

--- src/train.py
+++ src/train.py
@@ -88,7 +88,7 @@ def get_nets(obs_horizon=2):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Training script for the model.")
-    parser.add_argument("--num_epochs", type=int, default=10, help="Number of epochs to train the model.")
+    parser.add_argument("--num_epochs", type=int, default=200, help="Number of epochs to train the model.")
     parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate for the optimizer.")
     parser.add_argument("--batch_size", type=int, default=64, help="Batch size for the dataloader.")
     parser.add_argument("--diffusion_iters", type=int, default=100, help="Iteration of one diffusion step.")
@@ -102,7 +102,7 @@ if __name__ == "__main__":
     nets, ema = get_nets(obs_horizon=obs_horizon)
 
     dataloader = get_dataloader(pred_horizon=pred_horizon, obs_horizon=obs_horizon, action_horizon=action_horizon,
-                                batch_size=args.batch_size, small_dataset=True)
+                                batch_size=args.batch_size)
 
     optimizer = torch.optim.AdamW(params=nets.parameters(), lr=args.lr, weight_decay=1e-6)
 
